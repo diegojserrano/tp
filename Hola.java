fdb28f7a2b990489ea0eabf15d369cb8c651c137
Metalderly2023 <129208394+Metalderly2023@users.noreply.github.com>
2023-03-30 11:22:44 -0300

    cambiando el programa un toque

--- Hola.java
+++ Hola.java
@@ -1,5 +1,7 @@
 public class Hola {
 	public static void main(String[]args) {
-		System.out.println("Hola!!!, este es mi primer archivo del tp");	
+		System.out.println("Hola!!!, este es mi primer archivo del tp");
+		System.out.println("hola clase que ondix");
+		System.out.println("probando en clases");
 	}
 }
